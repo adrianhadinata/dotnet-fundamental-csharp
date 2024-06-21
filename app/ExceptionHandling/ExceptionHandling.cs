fbf8e33641befe2f6d7977795646064691c5c136
adrianhhd <118419967+adrianhhd@users.noreply.github.com>
2024-06-21 07:44:34 +0700

    Update: Exception Handling

---
+++ app/ExceptionHandling/ExceptionHandling.cs
@@ -0,0 +1,44 @@
+namespace FundamentalUpskilling;
+
+public class ExceptionHandling
+{
+    public static void Main(string[] args)
+    {
+        /*
+            Macam - macam exception:
+                -> NullReferenceException
+                -> IndexOutOfRangeException
+                -> ArgumentException
+                -> DivideByZeroException
+                -> OverflowException
+            
+            keyword untuk exception :
+                try, catch, finally, throw
+        */
+
+        // Divide by zero
+        
+        try 
+        {
+            int a = 10;
+            int b = 0;
+            int c = a / b;
+
+            Console.WriteLine(c);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+
+            //melempar kembali error
+            // throw;
+        }
+        finally
+        {
+            Console.WriteLine("Code setelah try catch");
+        }
+
+    }
+
+    
+}
